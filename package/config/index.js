6aff18f189597623dcae77c5407ef0dd1fc2adee
ErikYSH <Erikyang@Eriks-MacBook-Pro-2.local>
2022-02-08 21:06:50 -0800

    client and server ready for deployment

--- package/config/index.js
+++ package/config/index.js
@@ -1,3 +1,3 @@
-module.export = {
-    PORT: 4000,
+module.exports = {
+    PORT: process.env.PORT || 4000,
 };
--- server/server.js
+++ server/server.js
@@ -1,14 +1,29 @@
+require('dotenv').config
+/** External Modules **/
 const express = require('express');
+const path = require('path');
 
 /** Internal Modules **/
 
 /** Instance Modules **/
 const app = express();
 
+/** Configuration **/
 const config = require("@myflexspace/config");
 
-/** Routes & Controllers **/
+/** Middleware **/
+app.use(express.static(path.json("build")));
 
+/** Routes & Controllers **/
+app.all("/api/*", (req,res,next) => {
+    res.send("Hold UP These are not the apis YOU are Looking For")
+})
+
+
+// MAGICAL FULL STACK MIDDLEWARE (lowest level)
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"))
+});
 
 /** Server Lisneter **/
 app.listen(config.PORT, () => {
